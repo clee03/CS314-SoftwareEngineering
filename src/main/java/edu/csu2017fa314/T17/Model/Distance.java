e7229ddbb1659b2244b11767fbee544a8c6db43d
Thomas Shaw <shawtm@topeka.cs.colostate.edu>
2017-09-03 14:46:38 -0600

    fix #37

---
+++ src/main/java/edu/csu2017fa314/T17/Model/Distance.java
@@ -0,0 +1,14 @@
+package edu.csu2017fa314.T17.Model;
+
+public class Distance {
+    public double convertToRadian(double val)
+    {
+        return 0.0;
+    }
+
+    public int greatCircleDistance(Brewery alpha, Brewery beta)
+    {
+        return 0;
+    }
+
+}
