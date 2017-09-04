beaebff5e65ba259594b4bcf636d28e30d61764f
Tom Shaw <31258999+shawtm@users.noreply.github.com>
2017-09-04 11:28:07 -0600

    Fixed Review Changes
    Removed large comments and changed style to 2 spaces

--- src/main/java/edu/csu2017fa314/T17/Model/Distance.java
+++ src/main/java/edu/csu2017fa314/T17/Model/Distance.java
@@ -1,47 +1,27 @@
 package edu.csu2017fa314.T17.Model;
 
 public class Distance {
-    double radius;
-    public Distance()
-    {
-        radius = 6371.0088;
-        return;
-    }
+  double radius;
 
-    public double convertToRadians(double degrees)
-    {
-        return (degrees * (Math.PI/ 180.0));
-    }
+  public Distance() {
+    radius = 6371.0088;
+    return;
+  }
 
-    public int greatCircleDistance(Brewery alpha, Brewery beta)
-    {
-        int distance = 0;
-        double theta1 = convertToRadians(alpha.getLat());
-        double theta2 = convertToRadians(beta.getLat());
-        double delta = convertToRadians(alpha.getLon()) -
-                    convertToRadians(beta.getLon());
-        double subLower = (Math.sin(theta1) * Math.sin(theta2)) +
-                (Math.cos(theta1) * Math.cos(theta2) * Math.cos(delta));
-        double subUpper = Math.pow((Math.cos(theta2)*Math.sin(delta)),2) +
-                Math.pow(((Math.cos(theta1) * Math.sin(theta2)) - (Math.sin(theta1) * Math.cos(theta2) * Math.cos(delta))),2);
-        double angle = Math.atan2( Math.sqrt(subUpper), subLower);
-        distance = (int) Math.round(angle * this.radius);
-        return distance;
-    }
-    /*public int greatCircleDistance(Brewery alpha, Brewery beta)
-    {
-        int R = 6371; // metres
-        double φ1 = convertToRadians(alpha.getLat());
-        double φ2 = convertToRadians(beta.getLat());
-        double Δφ = convertToRadians(beta.getLat() - alpha.getLat());
-        double Δλ = convertToRadians(beta.getLon() - alpha.getLon());
+  public double convertToRadians(double degrees) {
+    return (degrees * (Math.PI / 180.0));
+  }
 
-        double a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
-                Math.cos(φ1) * Math.cos(φ2) *
-                        Math.sin(Δλ/2) * Math.sin(Δλ/2);
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-
-        double d = R * c;
-        return (int) d;
-    }*/
+  public double greatCircleDistance(Brewery alpha, Brewery beta) {
+    double theta1 = convertToRadians(alpha.getLat());
+    double theta2 = convertToRadians(beta.getLat());
+    double delta = convertToRadians(alpha.getLon()) -
+        convertToRadians(beta.getLon());
+    double subLower = (Math.sin(theta1) * Math.sin(theta2)) +
+        (Math.cos(theta1) * Math.cos(theta2) * Math.cos(delta));
+    double subUpper = Math.pow((Math.cos(theta2) * Math.sin(delta)), 2) +
+        Math.pow(((Math.cos(theta1) * Math.sin(theta2)) - (Math.sin(theta1) * Math.cos(theta2) * Math.cos(delta))), 2);
+    double angle = Math.atan2(Math.sqrt(subUpper), subLower);
+    return Math.round(angle * this.radius);
+  }
 }
--- src/test/java/edu/csu2017fa314/T17/Model/TestDistance.java
+++ src/test/java/edu/csu2017fa314/T17/Model/TestDistance.java
@@ -1,32 +1,31 @@
 package edu.csu2017fa314.T17.Model;
-import static org.junit.Assert.*;
 
-import org.junit.Before;
-import org.junit.Test;
+    import static org.junit.Assert.*;
+
+    import org.junit.Before;
+    import org.junit.Test;
 
 public class TestDistance {
-    private Distance d;
+  private Distance d;
 
 
-    @Before
-    public void setUp() throws Exception{
-        d = new Distance();
-    }
+  @Before
+  public void setUp() throws Exception {
+    d = new Distance();
+  }
 
 
-    @Test
-    public void testRadians()
-    {
-        assertEquals(d.convertToRadians(180.0), Math.PI, .01);
-    }
+  @Test
+  public void testRadians() {
+    assertEquals(d.convertToRadians(180.0), Math.PI, .01);
+  }
 
-    @Test
-    public void testDistance()
-    {
-        Brewery alpha = new Brewery("a1", "alpha", "alpha city",
-                50.06639, -5.714722, 5000);
-        Brewery beta = new Brewery("b1", "beta", "beta city",
-                58.64389, -3.07, 5000);
-        assertEquals(d.greatCircleDistance(alpha, beta), 968.9, 1);
-    }
+  @Test
+  public void testDistance() {
+    Brewery alpha = new Brewery("a1", "alpha", "alpha city",
+        50.06639, -5.714722, 5000);
+    Brewery beta = new Brewery("b1", "beta", "beta city",
+        58.64389, -3.07, 5000);
+    assertEquals(d.greatCircleDistance(alpha, beta), 968.9, 1);
+  }
 }
