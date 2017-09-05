e7e0a39cf781614a0fbf2f9f84667ab0aa90e24e
Livingwind <westerman.christopher@gmail.com>
2017-09-05 14:53:22 -0700

    Fix #32
    Added the function to alter the string/degree form into decimal

--- src/main/java/edu/csu2017fa314/T17/Model/ParseCSV.java
+++ src/main/java/edu/csu2017fa314/T17/Model/ParseCSV.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 public class ParseCSV{
   ArrayList<String> header;
-  Brewery[] brewlist;
+  ArrayList<Brewery> brewList;
 
   public ParseCSV( String filename ) throws Exception{
     // This loads from the resources folder ( src/main/resources or src/test/resources )
@@ -16,17 +16,56 @@ public class ParseCSV{
         new InputStreamReader( new FileInputStream( loadFile ) )
       );
 
-    parseHeader( in.readLine() );
-
-//    if( String[] colheader = in.readLine() != null ){
-//      String line;
-//      while( ( line = in.readLine() ) != null ){
-//      }
-//    }
+    String line;
+    if( ( line = in.readLine() ) != null ){
+      parseHeader( line.toLowerCase() );
+      while( ( line = in.readLine() ) != null ){
+        parseLine( line );
+      }
+    }
   }
-
+  
+  /**
+   *  Helper function to parse the first line of data
+   *  @param line       the line to be parsed ( comma seperated )
+   *  @return void      result added to the internal header list
+   */
   private void parseHeader( String line ){
     header = new ArrayList<String>( Arrays.asList( line.split(",") ) );
+  }
+
+  /**
+   *  Helper function to parse a line in the brew file according to the header
+   *  @param line       the line to be parsed ( comma seperated )
+   *  @return void      added to internal brew list
+   */
+  private void parseLine( String line ){
+  }
+
+  /**
+   *  Helper function to exchange lat/lon into their decimal counterparts
+   *  @param degrees    the string that will be parsed
+   *  @return double    a decimal value representing the string passed as a parameter
+   */
+  public double degToDecimal( String degrees ){
+    degrees = degrees.replace( " ", "" );
+    String[] parts = degrees.split( "[°'\"]" );
+
+    double total = 0;
+    switch( parts.length ){
+      case 4:
+        total += ( Double.parseDouble( parts[2] ) / 3600 );
+      case 3:
+        total += ( Double.parseDouble( parts[1] ) / 60 );
+      case 2:
+        total += ( Double.parseDouble( parts[0] ) );
+        String cardinal = parts[ parts.length - 1 ];
+        if( cardinal.equals("W") || cardinal.equals("S") )
+          total *= -1;
+        return total;
+      default:
+        return Double.parseDouble( parts[0] );
+    }
   }
 
   public ArrayList<String> getHeader(){
--- src/test/java/edu/csu2017fa314/T17/Model/TestParseCSV.java
+++ src/test/java/edu/csu2017fa314/T17/Model/TestParseCSV.java
@@ -16,11 +16,18 @@ public class TestParseCSV{
   @Test
   public void testHeaders(){
     ArrayList<String> tmp =
-      new ArrayList<String>( Arrays.asList( "ID","Name","City","Latitude","Longitude","ElevationFt" ) );
+      new ArrayList<String>( Arrays.asList( "id","name","city","latitude","longitude","elevationft" ) );
     assertEquals( parseObj.getHeader(), tmp );
   }
   @Test
   public void testData(){
-    
+    String testString1 = "106\u00B049'43.24\" W";
+    String testString2 = "39°38' N";
+    String testString3 = "40.98° W";
+    String testString4 = "-28.39";
+    assertEquals( parseObj.degToDecimal( testString1 ), -106.829, 0.001 );   
+    assertEquals( parseObj.degToDecimal( testString2 ), 39.633, 0.001 );
+    assertEquals( parseObj.degToDecimal( testString3 ), -40.98, 0.001 );
+    assertEquals( parseObj.degToDecimal( testString4 ), -28.39, 0.001 );
   }
 }
