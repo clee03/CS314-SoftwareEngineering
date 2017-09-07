565d72ac9dde6a8d83b7c1e3be5d20640783db46
Tom Shaw <31258999+shawtm@users.noreply.github.com>
2017-09-06 18:05:58 -0600

    Updated main!
    1) reformatted spacing to style guide
    2) add funcitonality to main program and dialogue describing usage. Can be Updated in the future easily.
    3) created commented method calls to unfinished mehtods.

--- src/main/java/edu/csu2017fa314/T17/TripCo.java
+++ src/main/java/edu/csu2017fa314/T17/TripCo.java
@@ -1,33 +1,43 @@
 package edu.csu2017fa314.T17;
-public class TripCo
-{
+import edu.csu2017fa314.T17.Model.*;
+import edu.csu2017fa314.T17.View.*;
+import java.util.ArrayList;
 
-   private String name = "";
+public class TripCo {
 
-   public String getName()
-   {
-      return name;
-   }
+  private String name = "";
 
-   public String getMessage()
-   {
-      if (name == "")
-      {
-         return "Hello!";
-      }
-      else
-      {
-         return "Hello " + name + "!";
-      }
-   }
+  public String getName() {
+    return name;
+  }
 
-   public void setName(String name)
-   {
-      this.name = name;
-   }
+  public String getMessage() {
+    if (name == "") {
+      return "Hello!";
+    } else {
+      return "Hello " + name + "!";
+    }
+  }
 
-   public static void main(String[] args) {
-       System.out.println("Welcome to TripCo");
-   }
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Welcome to TripCo");
+    if (args.length == 0){
+      System.out.println("Proper usage of this program is through " +
+          "the commandline followed by a .csv file");
+    }else if( args.length > 1){
+      System.out.println("sorry this feature isn't implemented yet." +
+          "please use only a single file for now");
+    }
+    Arraylist<Brewery> brewList = new ArrayList<Brewery>();
+    //brewList = ParseCSV(args[0]);
+    //JsonWriter jWrite = new JsonWriter(brewList);
+    //JsonWriter.write();
+    System.out.println("Json file successfully created!");
+
+  }
 
 }
