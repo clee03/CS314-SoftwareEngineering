2538c6c18cbbd14ddaf21d374935c405d6755420
Livingwind <westerman.christopher@gmail.com>
2017-11-02 11:12:39 -0600

    Fix #142 Set up the tables to hold searched and selected values. Currently remove buttons don't work.

--- web/src/components/Planner/index.jsx
+++ web/src/components/Planner/index.jsx
@@ -8,6 +8,29 @@ import './index.scss';
 class Planner extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      data: [
+        { code: 'a', name: 'a' },
+        { code: 'b', name: 'b' },
+        { code: 'c', name: 'a' },
+        { code: 'd', name: 'a' },
+        { code: 'e', name: 'a' },
+        { code: 'a', name: 'a' },
+      ],
+      selected: [
+      ]
+    };
+    this.handleAddClick = this.handleAddClick.bind(this);
+    this.handleRemoveClick = this.handleRemoveClick.bind(this);
+  }
+
+  handleAddClick(obj) {
+    let combined = this.state.selected.concat(obj);
+    this.setState({selected: combined});
+    console.log(combined);
+  }
+  handleRemoveClick(obj) {
+    console.log("Remove");
   }
 
   render() {
@@ -16,7 +39,14 @@ class Planner extends React.Component {
         <SearchBox
           handleSearch={this.props.handleSearch}
         />
-        <SearchResults/>
+        <SearchResults
+          data={this.state.data}
+          handleAddClick={this.handleAddClick}
+        />
+        <SelectedDestinations
+          selected={this.state.selected}
+          handleRemoveClick={this.handleRemoveClick}
+        />
       </div>
     );
   }
--- web/src/components/SelectedDestinations/index.jsx
+++ web/src/components/SelectedDestinations/index.jsx
@@ -1,13 +1,43 @@
 import React, {Component} from 'react';
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
+import './index.scss';
 
 class SelectedDestinations extends React.Component {
   constructor(props) {
     super(props);
   }
 
+  handleClick(state, rowInfo, column, instance) {
+    return {
+      onClick: e => {
+          if(rowInfo !== undefined &&
+            column.id === "remove" ){
+            console.log("Clicked remove on ", rowInfo.original);
+            instance.props.handleRemoveClick(rowInfo.original);
+            }
+        }
+    };
+  }
+
   render() {
     return (
-
+      <div id='search_results'>
+        <ReactTable
+          data={this.props.selected}
+          columns={[
+            { Header: 'Remove', accessor: 'remove', filterable: false,
+              width: 50, resizeable: false,  className: 'remove-column' },
+            { Header: 'Code', accessor: 'code', width: 100 },
+            { Header: 'Name', accessor: 'name' }
+          ]}
+          getTdProps={this.handleClick}
+          showPageSizeOptions={false}
+          defaultPageSize={10}
+          className="-striped"
+          handleRemoveClick={this.props.handleRemoveClick}
+        />
+      </div>
     );
   }
 }
