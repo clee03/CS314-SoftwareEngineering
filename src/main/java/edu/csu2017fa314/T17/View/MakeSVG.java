b29cf946662ae350ee1e6c70a81fa88eef5b77e8
Ian D Kirkpatrick <thehumoroushorse@gmail.com>
2017-11-04 21:04:53 -0600

    Toast s4 worldmapsvg (#190)
    * Fix #182
    Placed each component in its own file with corresponding scss file.
    Changed all the import statements to match the previous layout as well
    as moving the inline styles to their own stylesheets.
    
    * Fix #142
    Set up the tables to hold searched and selected values.
    Currently remove buttons don't work.
    
    * Fix #144
    Fix #140
    Fix #139
    
    Closes two epics with stage 2.  Front-end now allows selection of data and
    building of an itinerary.
    
    * Still passes giant string with map and svg but meh

--- src/main/java/edu/csu2017fa314/T17/View/MakeSVG.java
+++ src/main/java/edu/csu2017fa314/T17/View/MakeSVG.java
@@ -11,18 +11,20 @@ public class MakeSVG {
   private double ratiox;
   private double ratioy;
 
+  // Colorado: 1066.6073, 783.0824
   public MakeSVG () {
-    this(1066.6073, 783.0824);
+    this(1024, 512);
   }
 
+  // world: 1024 x 512
   public MakeSVG (double width, double height) {
     this.width = width;
     this.height = height;
 
     // Calculate the ratio to use between deg and cart
     // Both of these are hardcoded for the colorado map but can be changed later
-    ratiox = (1027-36)/(-102+109.05);
-    ratioy = (747-40)/(37-41);
+    ratiox = width / 360.0;  // Colorado: (1027-36)/(-102+109.05);
+    ratioy = height / 180.0;  //           (747-40)/(37-41);
   }
 
   /**
@@ -36,7 +38,7 @@ public class MakeSVG {
                       boolean lines, boolean dots){
     try {
       PrintWriter out = new PrintWriter(filename.substring(0, filename.length() - 3) + "svg");
-      out.print(buildColoradoMap(brews, lines, dots));
+      out.print(buildMap(brews, lines, dots));
       out.close();
     }
     catch (FileNotFoundException e) {
@@ -44,16 +46,19 @@ public class MakeSVG {
     }
   }
 
-  private String loadColoradoBackground() throws IOException {
+  /**
+   * Outputs the svg as a string with world.svg and lines/dots
+   * @param backgroundFilepath      filepath to background svg
+   */
+  private String loadMapBackground(String backgroundFilepath) throws IOException {
     StringBuilder backgroundSVG = new StringBuilder("");
-    String backgroundFileName = "data/colorado_map.svg";
+    String backgroundFileName = backgroundFilepath; // data/world.svg
     String line = null;
-
     try (BufferedReader br = new BufferedReader(new FileReader(backgroundFileName))){
-      int skipFirstThree = 3;
+      int skipToSVGTag = 2; // World: 2, Colorado 3
       while((line=br.readLine()) != null){
-        if (skipFirstThree < 3){
-          skipFirstThree++;
+        if (skipToSVGTag < 2){
+          ++skipToSVGTag;
           continue;
         }
         backgroundSVG.append(line);
@@ -74,12 +79,12 @@ public class MakeSVG {
    * @param lines     Turns on and off lines
    * @param dots      Turns on and off dots
    */
-  public String buildColoradoMap (ArrayList<Brewery> brews, boolean lines, boolean dots){
+  public String buildMap (ArrayList<Brewery> brews, boolean lines, boolean dots){
     StringBuilder map = new StringBuilder();
 
     // add colorado map background to svg
     try {
-      map.append(loadColoradoBackground());
+      map.append(loadMapBackground("data/world.svg"));
     } catch (IOException e) {
       e.printStackTrace();
     }
@@ -124,17 +129,60 @@ public class MakeSVG {
   }
 
   /**
+   * Helper function for createLine();
+   * determines how to wrap lines off an image
+   * @param x1 start position of line
+   * @param x2 end position of line
+   */
+  private int shouldWrap(int x1, int x2){
+    // 0 don't wrap, 1 wrap left, 2 wrap right
+    if ((x1-x2) > width/2) return 1;
+    else if ((x2-x1) > width/2) return 2;
+    else return 0;
+  }
+
+  /**
    * Part of SVG helpers
    * Creates a line connecting two Brewery's
    * @param brew1 First Brewery
    * @param brew2 Second Brewery
    */
   private String createLine (Brewery brew1, Brewery brew2) {
-    return String.format(
-        "<line y1='%d' x1='%d' y2='%d' x2='%d' stroke-width='1' stroke='#0000FF' fill='none'/>",
-        Math.round(degreesToCartY(brew1)), Math.round(degreesToCartX(brew1)),
-        Math.round(degreesToCartY(brew2)), Math.round(degreesToCartX(brew2))
-    ).replace('\'','\"');
+    int x1, x2;
+    int y1, y2;
+    x1 = (int)Math.round(degreesToCartX(brew1));
+    x2 = (int)Math.round(degreesToCartX(brew2));
+    y1 = (int)Math.round(degreesToCartY(brew1));
+    y2 = (int)Math.round(degreesToCartY(brew2));
+
+    String line;
+    System.out.println("Brew Start: " + brew1.get("name"));
+    System.out.println("Should wrap: " + shouldWrap(x1, x2));
+    if (shouldWrap(x1, x2) == 1) {
+      System.out.println("Drawing wrap");
+      line = String.format(
+          "<line y1='%d' x1='%d' y2='%d' x2='%d' stroke-width='1' stroke='#0000FF' fill='none'/>" +
+          "<line y1='%d' x1='%d' y2='%d' x2='%d' stroke-width='1' stroke='#0000FF' fill='none'/>",
+          y1, x1-(int)width, y2, x2,
+          y1, x1, y2, (int)width+x2
+      ).replace('\'', '\"');
+    }
+    else if (shouldWrap(x1, x2) == 2) {
+      System.out.println("Drawing wrap");
+      line = String.format(
+          "<line y1='%d' x1='%d' y2='%d' x2='%d' stroke-width='1' stroke='#0000FF' fill='none'/>" +
+              "<line y1='%d' x1='%d' y2='%d' x2='%d' stroke-width='1' stroke='#0000FF' fill='none'/>",
+          y1, (int)width+x1, y2, x2,
+          y1, x1, y2, x2-(int)width
+          ).replace('\'', '\"');
+    }
+    else { // shouldWrap == 0
+      line = String.format(
+          "<line y1='%d' x1='%d' y2='%d' x2='%d' stroke-width='1' stroke='#0000FF' fill='none'/>",
+          y1, x1, y2, x2
+      ).replace('\'', '\"');
+    }
+    return line;
   }
 
   /**
@@ -142,8 +190,10 @@ public class MakeSVG {
    * @return  value matching the cartesian map X
    */
   private double degreesToCartX (Brewery brew) {
-    double deltaLoc = (double) brew.get("longitude") + 109.05;
-    return deltaLoc * ratiox + 36;
+    double longitude = (double) brew.get("longitude");
+    // shift the map over to the right by 180
+    double deltaLoc = longitude + 180;
+    return deltaLoc * ratiox;
   }
 
   /**
@@ -151,8 +201,11 @@ public class MakeSVG {
    * @return value matching the cartesian map Y
    */
   private double degreesToCartY (Brewery brew) {
-    double deltaLoc = (double) brew.get("latitude") - 41;
-    return deltaLoc * ratioy + 40;
+    double latitude = (double) brew.get("latitude");
+    // shift the map down by 90
+    double deltaLoc = latitude + 90;
+    // flip it back to
+    return height - (deltaLoc * ratioy);
   }
 
 }
